bf0608cce9118e6092981413895fc74608b9a5ef
negarmiralaie <miralaienegar@gmail.com>
2023-07-08 15:03:16 +0330

    fixed route limiting

--- index.php
+++ index.php
@@ -13,29 +13,24 @@
         <?php
            error_reporting(E_ALL);
            ini_set('display_errors', 1);
-        
-            ini_set('display_errors', 1);
-            error_reporting(-1);             
             
             // CONNECT TO DB           
             include(__DIR__ . '/connect.php');
-        ?>
+            $request = $_SERVER['REQUEST_URI'];
+            // echo $request;
 
-<?php
-$request = $_GET['url'] ?? '/';
-
-switch ($request) {
-    case '/' :
-        // require __DIR__ . '/views/home.php';
-        break;
-    case '/about' :
-        require __DIR__ . '/a/views/about.php';
-        break;
-    default:
-        http_response_code(404);
-        require __DIR__ . '/a/views/404.php';
-        break;
-}
+            switch ($request) {
+                case '/' :
+                    // require __DIR__ . '/views/home.php';
+                    break;
+                case '/php-test-website/about' :
+                    require __DIR__ . '/views/about.php';
+                    break;
+                default:
+                    http_response_code(404);
+                    require __DIR__ . '/views/404.php';
+                    break;
+            }
 
 ?>
     </main>
